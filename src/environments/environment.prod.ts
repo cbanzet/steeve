ae64523431f83ffba2fd4f769aec3847eda53f89
Corentin <cbanzet@gmail.com>
2023-02-22 14:18:52 +0100

    config files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
-  production: true
-  
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCc4bRdTgaXJyFcsRV2M3QZVRPkxpRH1UQ",
+    authDomain: "steeve-531ff.firebaseapp.com",
+    projectId: "steeve-531ff",
+    storageBucket: "steeve-531ff.appspot.com",
+    messagingSenderId: "835192624511",
+    appId: "1:835192624511:web:8912677b7c2dbaa103764a",
+    measurementId: "G-JS7LW068VC"
+  }  
 };
