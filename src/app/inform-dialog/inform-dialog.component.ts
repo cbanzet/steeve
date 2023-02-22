ebf8c3ce3141e3590d9437a0142d768a97b19c44
Corentin <cbanzet@gmail.com>
2023-02-22 14:13:02 +0100

    update service and heroes categories tickets

---
+++ src/app/inform-dialog/inform-dialog.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-inform-dialog',
+  templateUrl: './inform-dialog.component.html',
+  styleUrls: ['./inform-dialog.component.scss']
+})
+export class InformDialogComponent implements OnInit {
+
+  public title: string = "";
+  public message: string = "";
+  public action: string = "";
+
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string, action: string, title: string }) { }
+
+  ngOnInit(): void {
+    this.message = this.data.message ?? "";
+    this.action = this.data.action ?? "Ok";
+    this.title = this.data.title ?? "";
+  }
+
+}
